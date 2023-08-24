a7f3984c89f4679f7c2b4d0a8e17471b51cb70ca
Gregório <arthurgregorioleal@gmail.com>
2023-08-24 18:45:50 -0300

    Feat: #7 adicionado suporte ao swagger

--- backend/FullstackAfiliados/src/main.ts
+++ backend/FullstackAfiliados/src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  Logger.log(
+    dotenv.config().error ? 'Dotenv file not found' : 'Using dotenv',
+    'Environment',
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Fullstack Afiliados')
+    .setDescription('Back-end Challenge - Fullstack Afiliados')
+    .setVersion('1.0')
+    .addTag('afiliados')
+    .setContact(
+      'Arthur Gregório',
+      'https://github.com/ogregorio/',
+      'arthurgregorioleal@mail.com',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(5001);
 }
 bootstrap();
