0a3a6f6e437e4e41b3dc7ff87dd70c4f43d164f8
Gregório <arthurgregorioleal@gmail.com>
2023-08-24 21:23:35 -0300

    Feat: #1 adicionada persistência das transações lidas do arquivo

--- backend/FullstackAfiliados/src/transactions/transactions.service.ts
+++ backend/FullstackAfiliados/src/transactions/transactions.service.ts
@@ -3,17 +3,41 @@ import { createReadStream } from 'fs';
 import { join } from 'path';
 import parseSalesFile from 'src/@helpers/file-parser';
 import streamToString from 'src/@helpers/stream-reader';
-import { Transaction } from 'src/@types/transaction.type';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TransactionEntity } from 'src/@entities/transaction.entity'; // Importe a entidade correta
+import { TransactionTypeEntity } from 'src/@entities/transaction-type.entity';
 
 @Injectable()
 export class TransactionsService {
-  async readFileContent(file: Express.Multer.File): Promise<Transaction[]> {
+  constructor(
+    @InjectRepository(TransactionEntity)
+    private readonly transactionRepository: Repository<TransactionEntity>,
+    @InjectRepository(TransactionTypeEntity)
+    private readonly transactionTypeRepository: Repository<TransactionTypeEntity>,
+  ) {}
+
+  async readFileContent(file: Express.Multer.File): Promise<void> {
     try {
       const stream = createReadStream(join(process.cwd(), file.path));
       const content = await streamToString(stream);
       const parsedTransactions = parseSalesFile(content);
 
-      return parsedTransactions;
+      for (const parsedTransaction of parsedTransactions) {
+        const { relativeType, date, product, amount, salesman } =
+          parsedTransaction;
+
+        const transaction = new TransactionEntity();
+        transaction.type = await this.transactionTypeRepository.findOne({
+          where: { type: relativeType },
+        });
+        transaction.date = date;
+        transaction.product = product;
+        transaction.amount = amount;
+        transaction.salesman = salesman;
+
+        await this.transactionRepository.save(transaction);
+      }
     } catch (error) {
       console.error('Error reading or parsing file:', error);
       throw error;
