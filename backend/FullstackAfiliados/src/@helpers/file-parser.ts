1022840c892f08aef66f5c627c940f1a7b61ee1f
Gregório <arthurgregorioleal@gmail.com>
2023-08-24 21:00:19 -0300

    Fix: #3 melhorias no tratamento de erro em arquivos inválidos

--- backend/FullstackAfiliados/src/@helpers/file-parser.ts
+++ backend/FullstackAfiliados/src/@helpers/file-parser.ts
@@ -7,13 +7,17 @@ function parseSalesFile(content: string): Transaction[] {
   try {
     for (const line of lines) {
       if (line.trim() !== '') {
-        transactions.push({
+        const transaction = {
           RelativeType: parseInt(line.substring(0, 1)),
           Date: new Date(Date.parse(line.substring(1, 26))),
           Product: line.substring(26, 56).trim(),
           Amount: parseFloat(line.substring(56, 66)) / 100,
           Salesman: line.substring(66).trim(),
-        });
+        };
+        if (isNotValid(transaction)) {
+          throw new Error('Failed at reading transactions file');
+        }
+        transactions.push(transaction);
       }
     }
   } catch (e) {
@@ -22,5 +26,9 @@ function parseSalesFile(content: string): Transaction[] {
 
   return transactions;
 }
+
+const isNotValid = (t: Transaction): boolean => {
+  return isNaN(t.Amount) || !(t.Date instanceof Date);
+};
 
 export default parseSalesFile;
